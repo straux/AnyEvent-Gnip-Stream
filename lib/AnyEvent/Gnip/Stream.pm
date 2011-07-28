4ac3fb6e1f41f29f53874b0144a648f3dca1ef28
Stéphane Raux <stephane.raux@linkfluence.net>
2011-07-28 12:18:01 +0200

    add POD

--- lib/AnyEvent/Gnip/Stream.pm
+++ lib/AnyEvent/Gnip/Stream.pm
@@ -107,10 +107,10 @@ sub new {
                     sub {
                         my ($handle, $headers) = @_;
                         return unless $handle;
-                        
+
                         my $chunk_reader = sub {
                             my ($handle, $line) = @_;
-                            
+
                             $line =~ /^([0-9a-fA-F]+)/ or die 'bad chunk (incorrect length)';
                             my $len = hex $1;
 
@@ -128,7 +128,7 @@ sub new {
                             my ($handle, $line) = @_;
                             $on_json_message->($line);
                         };
-                        
+
                         $handle->on_error(sub {
                             undef $handle;
                             $on_error->( $_[2] );
@@ -159,11 +159,121 @@ sub new {
                 );
             }
         );
-
     }
-
     return $self;
 }
 
 1;
+__END__
 
+=encoding utf-8
+
+=for stopwords
+API AnyEvent
+
+=for test_synopsis
+my($user, $password, @following_ids, $consumer_key, $consumer_secret, $token, $token_secret);
+
+=head1 NAME
+
+AnyEvent::Gnip::Stream - Receive Gnip Power Track streaming API in an event loop
+
+=head1 SYNOPSIS
+
+    use AnyEvent::Gnip::Stream;
+
+    # receive updates from Gnip Power Track
+    my $done = AE::cv;
+    my $listener = AnyEvent::Gnip::Stream->new(
+        username      => $user,
+        password      => $password,
+        gnip_box_name => $gnip_box_name,
+        collector_id  => $collector_id,
+        on_tweet      => sub {
+            my $tweet = shift;
+            print $tweet->{actor}->{preferredUsername}.": ".$tweet->{body}."\n";
+        },
+        on_error      => sub {
+            warn shift."\n";
+            $done->send;
+        },
+        on_connect    => sub {
+            print "Stream started!\n";
+        },
+        on_eof        => sub {
+            warn "EOF\n";
+            $done->send;
+        },
+        on_keepalive => sub {
+          warn "ping\n";
+        },
+        timeout => 60,
+    );
+    $done->recv;
+
+=head1 DESCRIPTION
+
+AnyEvent::Gnip::Stream is an AnyEvent user to receive Gnip Power Track streaming
+API, available at L<http://docs.gnip.com/w/page/35663947/Power-Track> and
+L<http://docs.gnip.com/w/page/37218164/Getting%20Started%20with%20Commercial%20Twitter%20Data>.
+
+See L<eg/track.pl> for more client code example.
+
+=head1 METHODS
+
+=head2 my $streamer = AnyEvent::Gnip::Stream->new(%args);
+
+=over 4
+
+=item B<username> B<password>
+
+These arguments are used for basic authentication.
+
+=item B<gnip_box_name> B<collector_id>
+
+These arguments are used to define the Gnip collector to link to.
+
+=item B<timeout>
+
+Set the timeout value.
+
+=item B<on_connect>
+
+Callback to execute when a stream is connected.
+
+=item B<on_tweet>
+
+Callback to execute when a new tweet is received.
+
+=item B<on_error>
+
+Callback to execute when an error occurs.
+
+=item B<on_eof>
+
+=item B<on_keepalive>
+
+=item B<no_decode_json>
+
+Set this param to TRUE if you don't want to deserialize json input from the stream.
+
+=head1 NOTES
+
+The API uses the HTTPS protocol. For this, you need to install the L<Net::SSLeay> module.
+
+=head1 AUTHOR
+
+Stéphane Raux E<lt>stephane.raux@linkfluence.netE<gt>
+
+(Based on Tatsuhiko Miyagawa's L<AnyEvent::Twitter::Stream>)
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+L<AnyEvent::Twitter::Stream>
+
+=cut
